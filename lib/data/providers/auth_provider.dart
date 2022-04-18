3e0a51a05a761c531764bd3301ae79bbbc68fdfb
michaellimair <michael@eventxtra.com>
2022-04-18 17:19:14 +0700

    fix: remove stored credentials on logout

--- lib/data/providers/auth_provider.dart
+++ lib/data/providers/auth_provider.dart
@@ -1,11 +1,14 @@
 import 'package:belarasa_mobile/routes/pages.dart';
 import 'package:belarasa_mobile/services/cookie_service.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get/get.dart';
 
 class AuthProvider extends GetConnect {
   Future<void> logout() async {
     CookieService cookieService = Get.find<CookieService>();
+    FlutterSecureStorage secureStorage = const FlutterSecureStorage();
     await cookieService.clearCookies();
+    await secureStorage.deleteAll();
     Get.offAllNamed(Routes.login);
   }
 }
